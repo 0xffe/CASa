f85f1d6eae4f94149e90ef9b9de022b46924a690
0xffe <32737168+0xffe@users.noreply.github.com>
2017-10-13 16:32:22 +0200

    Create add_mysql_users.sql
    initial scrypt

---
+++ core/www/add_mysql_users.sql
@@ -0,0 +1,4 @@
+INSERT INTO `user`(`Host`, `User`, `Password`, `Select_priv`, `Insert_priv`, `Update_priv`, `Delete_priv`, `Alter_priv`) 
+VALUES ('localhost', 'app_user', PASSWORD('app_user'), 'Y', 'Y', 'Y', 'Y', 'Y');
+INSERT INTO `user`(`Host`, `User`, `Password`, `Select_priv`, `Insert_priv`, `Update_priv`, `Delete_priv`, `Alter_priv`) 
+VALUES ('127.0.0.1', 'app_user', PASSWORD('app_user'), 'Y', 'Y', 'Y', 'Y', 'Y');
